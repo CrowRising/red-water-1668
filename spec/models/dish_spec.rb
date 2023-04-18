ff09577fe16ff5a7bdf9e8d0668f098f4566786d
Crow Rising <miscbyrd@gmail.com>
2023-04-18 14:50:41 -0600

    test: add test for class method total calories

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Dish, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+
+  describe "#class methods" do
+    describe "total_calories" do
+      it 'can calculate the total calories for a dish' do
+
+        expect(@dish_1.total_calories).to eq(140)
+      end
+    end
+  end
 end
